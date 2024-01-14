1a64c796e771ee4c994d2a57470ff014fe068e9a
Rohan Sunarya <144041284+rohansunarya@users.noreply.github.com>
2024-01-14 13:16:19 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Nav from './components/Nav';
+import Herosection from './components/hero';
+
+const App = ()=>{
+return <div>
+    <Nav/>
+    <Herosection/>
+</div> 
+
+};
+export default App;
---
+++ src/components/Nav.jsx
@@ -0,0 +1,23 @@
+
+const Nav = ()=>{
+
+    return(<div>
+<nav className="container">
+        <div className="logo">
+        <img src="https://ongpng.com/wp-content/uploads/2023/07/nike-logo.png" alt="" />
+        </div>
+        <ul>
+            <li href="#">Menu</li>
+            <li href="#">Location</li>
+            <li href="#">About</li>
+            <li href="#">Contact</li>
+        </ul>
+        <button className="login">Login</button>
+</nav>
+
+    </div>
+
+
+    )
+};
+export default Nav;
